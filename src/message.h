c357abe6dee7c46a359e5476853d3ccccf0aadb3
thomas-picariello <thpica@gmail.com>
2014-04-13 20:24:04 +0200

    Documentation of the Message class

--- src/message.h
+++ src/message.h
@@ -1,3 +1,10 @@
+/**
+ * @brief Message represent a message with a type.
+ *
+ * The type property should be used to properly display or treat the message.
+ * Once created neither the content nor the type od the message can be changed.
+ **/
+
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
@@ -7,11 +14,28 @@
 class Message
 {
 public:
+    /**
+     * @brief The TYPE enum lists the different message types
+     */
     enum TYPE{SENT, RECIEVED, SERVICE, ERR, UNDEF};
 
+    /**
+     * @brief Default constructor of class, the text and the type of the message are set here.
+     * @param text the text of the message
+     * @param type the type of the message
+     */
     Message(QString text, TYPE type);
 
+    /**
+     * @brief Returns the text of the message
+     * @return the text of the message
+     */
     const QString text();
+
+    /**
+     * @brief Returns the type of the message
+     * @return the type of the message
+     */
     const TYPE type();
 
 private:
