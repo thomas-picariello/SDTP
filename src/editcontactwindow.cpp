f2aa56f2eb221586d8a17aa940ed94154b792a81
thomas-picariello <thpica@gmail.com>
2014-04-16 19:20:42 +0200

    Contact key displayed as base 64 in EditContactWindow

--- src/editcontactwindow.cpp
+++ src/editcontactwindow.cpp
@@ -20,7 +20,7 @@ EditContactWindow::EditContactWindow(Contact *contact, ContactDB *contactDB, QWi
         item->setFlags(item->flags()|Qt::ItemIsEditable);
     }
     ui->port->setText(QString::number(mContact->getPort()));
-    ui->key->setText(QString(mContact->getKey()));
+    ui->key->setText(QString(mContact->getKey().toBase64()));
 
     mPortValidator.setRange(0, 65535);
     ui->port->setValidator(&mPortValidator);
@@ -52,13 +52,13 @@ void EditContactWindow::save(){
     QString port = ui->port->text();
     QString key = ui->key->toPlainText();
 
-    if(name.isEmpty() || hostsList.isEmpty() || port.isEmpty() || key.isEmpty()){
+    if(name.isEmpty() || hostsList.isEmpty() || port == 0 || key.isEmpty()){
         QMessageBox::warning(this, tr("Incomplete"), tr("Please fill all the fields"));
     }else{
         mContact->setName(name);
         mContact->setPort(port.toUInt());
         mContact->setHostsList(hostsList);
-        mContact->setKey(key.toUtf8());
+        mContact->setKey(QByteArray::fromBase64(key.toUtf8()));
         mContactDB->write(mContact);
         emit contactChanged();
         close();
