5ca565b239a1c8c83f44a9d7f391d7fbdc069387
thomas-picariello <thpica@gmail.com>
2014-07-03 17:16:36 +0200

    PacketCounter at link level

--- src/abstractlink.h
+++ src/abstractlink.h
@@ -17,10 +17,15 @@ public:
 
     AbstractLink(QObject *parent=0):
         QIODevice(parent),
-        m_State(Offline)
+        m_State(Offline),
+        m_RecievedPacketCount(0),
+        m_SentPacketCount(0)
     {}
 
     inline State state(){ return m_State; }
+    inline quint8 getRecievedPacketCount() const{ return m_RecievedPacketCount; }
+    inline quint8 getSentPacketCount() const{ return m_SentPacketCount; }
+
     virtual bool isSequential(){ return true; }
     virtual QString getHost() const = 0;
     virtual quint16 getPort() const = 0;
@@ -32,14 +37,20 @@ signals:
 
 
 public slots:
+    void incrementRecievedPacketCount(){ m_RecievedPacketCount++; }
     virtual void onConnected(){ setOpenMode(ReadWrite); emit stateChanged(Online); }
     virtual void onConnectionError(QAbstractSocket::SocketError) = 0;
 
 protected :
     State m_State;
+    quint8 m_RecievedPacketCount;
+    quint8 m_SentPacketCount;
 
-    qint64 readData(char * data, qint64 maxSize) = 0;
-    qint64 writeData(const char * data, qint64 size) = 0;
+    virtual qint64 readData(char * data, qint64 maxSize) = 0;
+    virtual qint64 writeData(const char * data, qint64 size) = 0;
+
+private:
+
 
     Q_DISABLE_COPY(AbstractLink)
 };
