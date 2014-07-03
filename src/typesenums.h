918e962c46bdd89f3ea02171b6dd418d32be8685
thomas-picariello <thpica@gmail.com>
2014-07-03 17:12:55 +0200

    New Undefined AppType and no more RootApp

--- src/typesenums.h
+++ src/typesenums.h
@@ -1,12 +1,16 @@
 #ifndef IDSENUMS_H
 #define IDSENUMS_H
 
+#include <QtGlobal>
+
 enum AppType: quint8{
-    Root = 0x00,
+    //0x00 is reseved for AppManager routing
     Messenger = 0x01,
     VoIP = 0x02,
     VideoStreamer = 0x03,
-    DataTransfer = 0x04
+    DataTransfer = 0x04,
+
+    Undefined = 0xFF    //reserved default
 };
 
 enum LinkType: quint8{
