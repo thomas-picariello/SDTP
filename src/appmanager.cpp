6b1c2d3216e5cffc5ec7e4e6f2b7dfe2d6a25f3e
thomas-picariello <thpica@gmail.com>
2014-07-08 20:00:55 +0200

    Mostly complete appManager

--- src/appmanager.cpp
+++ src/appmanager.cpp
@@ -1,31 +1,60 @@
 #include "appmanager.h"
 
-AppManager::AppManager(QObject* parent):
-    QObject(parent)
-{
-
-}
-
 void AppManager::readIncommingData(QByteArray &data){
+    Packet packet;
+    QDataStream (&data, QIODevice::ReadOnly) >> packet;
+    qDebug()<<packet.command << packet.distantUID.type() << packet.distantUID.instanceID();
+    switch(packet.command){
+    case StartAppCommand:
+        //emit startApp(packet.distantUID.type());
+        break;
+    case AppRegisterCommand:
+        registerConnection(packet.localUID, packet.distantUID);
+        break;
+    case AppRegisteredSignal:
 
+        break;
+    case AppUnregisteredSignal:
+        emit distantAppUnregistered(getLocalAppUID(packet.distantUID));
+        unregisterApp(packet.distantUID);
+        break;
+    default:
+        emit error(UnknownCommand);
+    }
 }
 
-void AppManager::startApp(AppType appType){
-
+void AppManager::requestPartnerApp(AppUID localAppUID){
+    Packet packet(StartAppCommand, localAppUID, AppUID());
+    QByteArray data;
+    QDataStream(&data, QIODevice::WriteOnly) << packet;
+    emit sendData(TCP, data);
 }
 
-void AppManager::requestPartnerApp(AppType){
-
-}
-
-void AppManager::registerApp(AppUID localAppUID, AbstractApp* app){
-
+void AppManager::registerApp(AppUID localAppUID, AbstractApp *app){
+    m_LocalAppsRegister.insert(localAppUID, app);
 }
 
 void AppManager::unregisterApp(AppUID localAppUID){
-
+    m_AppConnectionsTable.remove(localAppUID);
+    m_LocalAppsRegister.remove(localAppUID);
 }
 
-AppManager::~AppManager(){
+void AppManager::registerConnection(AppUID localAppUID, AppUID distantAppUID){
+    m_AppConnectionsTable.insert(localAppUID, distantAppUID);
+}
 
+QDataStream& operator<<(QDataStream &out, const AppManager::Packet &packet){
+    out << static_cast<quint8>(packet.command);
+    out << packet.distantUID;   //distant and local UID inverted here
+    out << packet.localUID;     //              ''
+    return out;
+}
+
+QDataStream& operator>>(QDataStream &in, AppManager::Packet &packet){
+    quint8 cmd;
+    in >> cmd;
+    packet.command = static_cast<AppManager::Command>(cmd);
+    in >> packet.localUID;
+    in >> packet.distantUID;
+    return in;
 }
--- src/appmanager.h
+++ src/appmanager.h
@@ -12,21 +12,65 @@ class AppManager : public QObject
 {
     Q_OBJECT
 public:
-    explicit AppManager(QObject* parent = 0);
-    ~AppManager();
+    Q_ENUMS(AppType)
+
+    enum Command: quint8{
+        StartAppCommand = 0x00,
+        AppRegisterCommand = 0x01,
+        AppRegisteredSignal = 0x02,
+        AppUnregisteredSignal = 0x03,
+
+        InvalidCommand = 0xFF
+    };
+    Q_ENUMS(Command)
+
+    enum Error{
+        PacketCorrupted,
+        UnknownCommand
+    };
+    Q_ENUMS(Error)
+
+    struct Packet{
+        Packet(){}
+        Packet(Command cmd, AppUID local, AppUID distant): command(cmd), localUID(local), distantUID(distant){}
+        Command command;
+        AppUID localUID;
+        AppUID distantUID;
+    };
+
+    explicit AppManager(QObject* parent = 0): QObject(parent){}
+
+    AbstractApp* getApp(AppUID uid)const            { return m_LocalAppsRegister.value(uid, NULL); }
+    AppUID getLocalAppUID(AbstractApp* app) const   { return m_LocalAppsRegister.key(app); }
+    AppUID getLocalAppUID(AppUID distantAppUID)const{ return m_AppConnectionsTable.key(distantAppUID); }
+    AppUID getDistantAppUID(AppUID localAppUID)const{ return m_AppConnectionsTable.value(localAppUID); }
+    AppUID getDistantAppUID(AbstractApp* app)const  { return getDistantAppUID(getLocalAppUID(app)); }
+    bool isAppRegistered(AbstractApp* app)const     { return (m_LocalAppsRegister.key(app).type() != Undefined); }
+    bool isAppConnected(AppUID distantAppUID)const  { return (m_AppConnectionsTable.key(distantAppUID).type() != Undefined); }
 
 public slots:
     void readIncommingData(QByteArray &data);
-    void startApp(AppType appType);
-    void requestPartnerApp(AppType);
+    void requestPartnerApp(AppUID localAppUID);
     void registerApp(AppUID localAppUID, AbstractApp *app);
     void unregisterApp(AppUID localAppUID);
 
 signals:
-    void sendData(QByteArray& data);
+    void distantAppUnregistered(AppUID distantUID);
+    void error(Error);
+    void sendData(LinkType linkType, QByteArray& data);
+    void startApp(AppType appType);
+
 
 private:
-    QMap<AppUID, AppUID> m_AppConnectionTable;
+    QMap<AppUID, AppUID> m_AppConnectionsTable;
+    QMap<AppUID, AbstractApp*> m_LocalAppsRegister;
+
+    void registerConnection(AppUID localAppUID, AppUID distantAppUID);
+
+    Q_DISABLE_COPY(AppManager)
 };
+
+QDataStream& operator<<(QDataStream &out, const AppManager::Packet &packet);
+QDataStream& operator>>(QDataStream &in, AppManager::Packet &packet);
 
 #endif // APPMANAGER_H
