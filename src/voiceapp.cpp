55109d898b497f669a0127c73099e3cd169da72b
thomas-picariello <thpica@gmail.com>
2014-07-18 12:59:45 +0200

    VoiceApp member refactoring

--- src/voiceapp.cpp
+++ src/voiceapp.cpp
@@ -2,10 +2,10 @@
 #include"ui_voiceapp.h"
 
 VoiceApp::VoiceApp(Contact* contact, QWidget* parent) :
-    AbstractApp(contact, parent), ui(new Ui::VoiceApp())
+    AbstractApp(contact, parent), m_ui(new Ui::VoiceApp())
 {
     setWindowFlags(Qt::WindowCloseButtonHint);
-    ui->setupUi(this);
+    m_ui->setupUi(this);
     layout()->setSizeConstraint(QLayout::SetFixedSize);
 
     if(contact->getStatus() != Contact::Offline)
@@ -14,8 +14,8 @@ VoiceApp::VoiceApp(Contact* contact, QWidget* parent) :
         m_state = Disconnected;
     onStateChange();
 
-    ui->mic_monitor_pb->setInterpolationTime(MONITORS_UPDATE_INTERVAL);
-    ui->output_monitor_pb->setInterpolationTime(MONITORS_UPDATE_INTERVAL);
+    m_ui->mic_monitor_pb->setInterpolationTime(MONITORS_UPDATE_INTERVAL);
+    m_ui->output_monitor_pb->setInterpolationTime(MONITORS_UPDATE_INTERVAL);
     connect(&m_monitorUpdateTimer, &QTimer::timeout, this, &VoiceApp::updateMonitorsValue);
     m_monitorUpdateTimer.start(MONITORS_UPDATE_INTERVAL);
 }
@@ -43,36 +43,36 @@ void VoiceApp::startCall(){
 }
 
 void VoiceApp::updateMonitorsValue(){
-    ui->mic_monitor_pb->setValue(qrand() % (ui->mic_vol_sl->value()+1));
-    ui->output_monitor_pb->setValue(qrand() % (ui->output_vol_sl->value()+1));
+    m_ui->mic_monitor_pb->setValue(qrand() % (m_ui->mic_vol_sl->value()+1));
+    m_ui->output_monitor_pb->setValue(qrand() % (m_ui->output_vol_sl->value()+1));
 }
 
 void VoiceApp::onStateChange(){
     QColor buttonColor;
-    disconnect(ui->call_bt, &QPushButton::clicked, this, 0);
+    disconnect(m_ui->call_bt, &QPushButton::clicked, this, 0);
     switch(m_state){
     case Disconnected:
         buttonColor.setNamedColor("#bbb");
-        ui->call_bt->setIcon(QIcon(":/icons/call"));
+        m_ui->call_bt->setIcon(QIcon(":/icons/call"));
         break;
     case Ready:
         buttonColor.setNamedColor("#0b0");
-        ui->call_bt->setIcon(QIcon(":/icons/call"));
-        connect(ui->call_bt, &QPushButton::clicked, this, &VoiceApp::startCall);
+        m_ui->call_bt->setIcon(QIcon(":/icons/call"));
+        connect(m_ui->call_bt, &QPushButton::clicked, this, &VoiceApp::startCall);
         break;
     case Calling:
         buttonColor.setNamedColor("#b00");
-        ui->call_bt->setIcon(QIcon(":/icons/hangup"));
-        connect(ui->call_bt, &QPushButton::clicked, this, &VoiceApp::endCall);
+        m_ui->call_bt->setIcon(QIcon(":/icons/hangup"));
+        connect(m_ui->call_bt, &QPushButton::clicked, this, &VoiceApp::endCall);
         break;
     }
-    ui->call_bt->setStyleSheet("QPushButton{background:"         + buttonColor.name() +";}"
+    m_ui->call_bt->setStyleSheet("QPushButton{background:"         + buttonColor.name() +";}"
                                "QPushButton::hover{background:"  + buttonColor.lighter(110).name() +";}"
                                "QPushButton::pressed{background:"+ buttonColor.darker(110).name() +";}");
 
     //update contact names color
     QLayoutItem *item;
-    while((item = ui->names_layout->takeAt(0)) != 0)
+    while((item = m_ui->names_layout->takeAt(0)) != 0)
         delete item->widget();
 
     foreach(Contact* contact, m_contactList){
@@ -87,11 +87,11 @@ void VoiceApp::onStateChange(){
         font.setPointSize(12);
         label->setFont(font);
         label->setAlignment(Qt::AlignHCenter);
-        ui->names_layout->addWidget(label);
+        m_ui->names_layout->addWidget(label);
     }
     update();
 }
 
 VoiceApp::~VoiceApp(){
-    delete ui;
+    delete m_ui;
 }
--- src/voiceapp.h
+++ src/voiceapp.h
@@ -4,6 +4,7 @@
 #include <QLabel>
 #include <QTimer>
 
+#include "opusvoicecodec.h"
 #include "abstractapp.h"
 
 namespace Ui{
@@ -14,6 +15,7 @@ class VoiceApp : public AbstractApp
 {
     Q_OBJECT
 public:
+    static const AppType APPTYPE = VoIP;
     static const int MONITORS_UPDATE_INTERVAL = 200;
 
     enum State{
@@ -37,7 +39,7 @@ private slots:
     void updateMonitorsValue();
 
 private:
-    Ui::VoiceApp *ui;
+    Ui::VoiceApp* m_ui;
     QTimer m_monitorUpdateTimer;
     State m_state;
 
